442741bc89b246b3149e902b315f67239d1723c8
june12btm <vibhanh.xworkz@gmail.com>
2023-06-27 21:23:19 +0530

    hi

---
+++ Bike.java
@@ -0,0 +1,9 @@
+class Bike{
+	public static void main(String[]args){
+		System.out.println("KTM");
+		System.out.println("DUKE");
+		System.out.println("RX100");
+		System.out.println("Super Bike");
+		System.out.println("Bullet");
+	}
+}
